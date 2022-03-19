e31be29d6ef4a5be6c25b3f4e8718b62d44420ce
rati200 <rati.archuadze2007@gmail.com>
2022-03-19 23:35:05 +0400

    1

--- Assets/script.cs
+++ Assets/script.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class script : MonoBehaviour
-{ 
+{ public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +17,16 @@ public class script : MonoBehaviour
     {
         
         if(Input.GetKey(KeyCode.D)){
-                  transform.Translate(Vector2.right * 0.05f);}
+                  transform.Translate(Vector2.right * speed);}
         
         if(Input.GetKey(KeyCode.A)){
-             transform.Translate(Vector2.left * 0.05f);}
+             transform.Translate(Vector2.left * speed);}
         
         if(Input.GetKey(KeyCode.W)){
-             transform.Translate(Vector2.up * 0.05f);}
+             transform.Translate(Vector2.up * speed);}
 
          if(Input.GetKey(KeyCode.S)){
-             transform.Translate(Vector2.down * 0.05f);}
+             transform.Translate(Vector2.down * speed);}
     
         
 
