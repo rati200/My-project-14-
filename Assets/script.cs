cefb40a75717641ffffd596ffac3a9818bf4f8ff
rati200 <rati.archuadze2007@gmail.com>
2022-03-30 16:37:43 +0400

    ??

--- Assets/script.cs
+++ Assets/script.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class script : MonoBehaviour
-{ public float speed;
+{   
+
+   [SerializeField] private GameObject go;
+
+     
+
+      [SerializeField] private Vector2 v2;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
-        
+      
         
     }
 
@@ -16,19 +24,8 @@ public class script : MonoBehaviour
     void Update()
     {
         
-        if(Input.GetKey(KeyCode.D)){
-                  transform.Translate(Vector2.right * speed);}
-        
-        if(Input.GetKey(KeyCode.A)){
-             transform.Translate(Vector2.left * speed);}
-        
-        if(Input.GetKey(KeyCode.W)){
-             transform.Translate(Vector2.up * speed);}
-
-         if(Input.GetKey(KeyCode.S)){
-             transform.Translate(Vector2.down * speed);}
-    
-        
+    if(Input.GetKey(KeyCode.Space)){
+            Instantiate(go, v2, Quaternion.identity);}
 
         
 
